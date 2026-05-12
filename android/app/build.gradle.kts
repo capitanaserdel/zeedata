4d271f9cbbb41c8c667aff7630db3edb563b8d8c
mac <gambosalma31@gmail.com>
2026-05-12 09:40:50 +0100

    vtu

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -6,7 +8,7 @@ plugins {
 }
 
 android {
-    namespace = "com.zeedata.frontend"
+    namespace = "com.zeedata.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -16,12 +18,12 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        jvmTarget = "17"
     }
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.zeedata.frontend"
+        applicationId = "com.zeedata.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -30,11 +32,29 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            val keyProperties = Properties()
+            val keyPropertiesFile = project.rootProject.file("key.properties")
+            if (keyPropertiesFile.exists()) {
+                keyProperties.load(keyPropertiesFile.inputStream())
+            }
+
+            keyAlias = keyProperties.getProperty("keyAlias")
+            keyPassword = keyProperties.getProperty("keyPassword")
+            storeFile = keyProperties.getProperty("storeFile")?.let { path -> project.rootProject.file(path) }
+            storePassword = keyProperties.getProperty("storePassword")
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
+            
+            // Optimization for production
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
 }
--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,5 +1,5 @@
 class AppConstants {
-  static const String baseUrl = 'https://ploy-preseason-joyous.ngrok-free.dev';
+  static const String baseUrl = 'https://api.zeedata.com.ng';
   static const String apiUrl = '$baseUrl/api';
   static const String storageUrl = '$baseUrl/storage/';
 }
